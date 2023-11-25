c46bc521e93df2b0a14f122b0dcd1825a5d618b6
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-11-25 15:37:10 +0330

    ✨ feat: category model

---
+++ src/modules/category/category.model.js
@@ -0,0 +1,22 @@
+const { Schema, Types, model } = require("mongoose");
+
+const CategorySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, index: true },
+    icon: { type: String, required: true },
+    parent: { type: Types.ObjectId, ref: "Category", required: false },
+    parents: { type: [Types.ObjectId], ref: "Category", required: false, default: [] },
+  },
+  { toJSON: { virtuals: true }, versionKey: false, id: false }
+);
+
+CategorySchema.virtual("children", {
+  ref: "Category",
+  localField: "-id",
+  foreignField: "parent",
+});
+
+const CategoryModel = model("category", CategorySchema);
+
+module.exports = { CategoryModel };
