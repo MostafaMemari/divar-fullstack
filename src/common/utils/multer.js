4d2280e5ac2a044725c4f4faa047a67385ff75d9
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-12-03 21:12:02 +0330

    ✨ feat: config multer

--- src/common/utils/multer.js
+++ src/common/utils/multer.js
@@ -1,5 +1,30 @@
+const fs = require("fs");
+const createHttpError = require("http-errors");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    fs.mkdirSync(path.join(process.cwd(), "public", "upload"), { recursive: true });
+    cb(null, "public/upload");
+  },
+  filename: function (req, file, cb) {
+    const whiteListFormat = ["image/png", "image/jpg", "image/jpeg", "image/webp"];
+    if (whiteListFormat.includes(file.mimetype)) {
+      const ext = path.extname(file.originalname);
+      const filename = new Date().getTime().toString() + ext;
+      cb(null, filename);
+    } else {
+      cb(createHttpError.BadRequest("format of picture are wrong!"));
+    }
+  },
+});
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 3 * 1000 * 1000,
+  },
+});
 
 module.exports = {
   upload,
